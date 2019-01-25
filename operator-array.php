85002bcb2a6381da93c664cd298d09285a4e2f3f
summerine <indri.pr.26@gmail.com>
2019-01-26 00:06:21 +0700

    add operator-array

---
+++ operator-array.php
@@ -0,0 +1,10 @@
+<?php
+$a = array("a" => "apple", "b" => "banana");
+$b = array("a" => "apricot", "b" => "Blackberry", "c" => "Coconut" );
+
+// joining $a and $b
+$c = $a + $b;
+
+print_r($c);
+
+?>
