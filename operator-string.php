e800d916d79004e18483cb4e6572d45f37c1677d
summerine <indri.pr.26@gmail.com>
2019-01-25 23:38:09 +0700

    initial commit

---
+++ operator-string.php
@@ -0,0 +1,14 @@
+<?php 
+
+$s1 = "PHP ";
+$s2 = " Programming";
+
+//using the operator . 
+$s3 = $s1 . $s2;
+print "The value of s3: " . $s3 . "<br>";
+
+// using the operator .=
+$s1 .= $s2;
+print "The value of s1: ". $s1 . "<br>";
+
+ ?>
